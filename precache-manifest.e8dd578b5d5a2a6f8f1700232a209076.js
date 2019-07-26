a9f9088394c0b541eaa92263c416d7242e3a0ec3
Marat <yeffasol@mail.ru>
2019-07-26 13:57:24 +0300

    Updates

---
+++ precache-manifest.e8dd578b5d5a2a6f8f1700232a209076.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00286c59aa043f902000526471057f19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15bf957d7dce97a620b6",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "34021e4fbe6eb16663fb",
+    "url": "./static/css/main.32b921ff.chunk.css"
+  },
+  {
+    "revision": "15bf957d7dce97a620b6",
+    "url": "./static/js/2.f9a72fd8.chunk.js"
+  },
+  {
+    "revision": "34021e4fbe6eb16663fb",
+    "url": "./static/js/main.ddda2e3f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6d0035075f89b0eb291453ad66afc16a",
+    "url": "./static/media/grid.6d003507.svg"
+  },
+  {
+    "revision": "2c20d9c8817532631783abf3ab42a453",
+    "url": "./static/media/list.2c20d9c8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-shop/precache-manifest.6bbf7755fc645f1266a24b94b1a86508.js"
+  "./precache-manifest.e8dd578b5d5a2a6f8f1700232a209076.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-shop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
