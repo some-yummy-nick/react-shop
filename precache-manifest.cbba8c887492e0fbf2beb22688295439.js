0069174d8b4365f9faba2d26bb2412d0ef17f249
Marat <yeffasol@mail.ru>
2019-07-26 15:13:55 +0300

    Updates

---
+++ precache-manifest.cbba8c887492e0fbf2beb22688295439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c070df596c4732a4cf178918394bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ea5bd3504beea4af63b",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "bff83dfffe4e5c4b8a55",
+    "url": "/static/css/main.f4c4c424.chunk.css"
+  },
+  {
+    "revision": "4ea5bd3504beea4af63b",
+    "url": "/static/js/2.d7dc27c1.chunk.js"
+  },
+  {
+    "revision": "bff83dfffe4e5c4b8a55",
+    "url": "/static/js/main.4409d7ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
