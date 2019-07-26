a529bdb25a67976514be7e22fe8fa7b274f823ae
Marat <yeffasol@mail.ru>
2019-07-26 15:06:12 +0300

    Update link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route , Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 import Main from './containers/Main';
@@ -12,14 +12,13 @@ const render = () => ReactDOM.render(
         <div className="shop">
             <div className="container">
                 <header className="header">
-                    <Link to="/main">
+                    <Link to="/">
                         <div className="shop__main">Главная</div>
                     </Link>
                     <h1 className="shop__title">Магазин</h1>
                 </header>
                 <div className="shop__wrapper">
                     <Route exact path="/" component={Main}/>
-                    <Route path="/main" component={Main}/>
                     <Route path="/product/:id" component={Product}/>
                 </div>
             </div>
